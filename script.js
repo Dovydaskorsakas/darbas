4e1cb973ec206f73511b58384c34e08032d4cb1c
Dovydaskorsakas <dovydas.korsakas@stud.kitm.lt>
2024-05-16 13:41:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let miktukas = document .querySelector("miktukas");
+let list = document .querySelector();
+let pridet = document .querySelector();
+let task = entertask.value;
+
+
+miktukas .addEventLisner("click", () =>{
+    list = document. createElement('ul')
+    list.getAtributeByID("class, inline")
+checkbox = document.createElement("input")
+checkbox.setAtribute ("type, checkbox")
+checkbox. setAtribute("class, inline")
+document. getElementByID("sdfsdf"). appendChlid(checkbox);
+list.inerText = task;
+})
